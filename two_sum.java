5f92a347ba208a10e6ff13fcbec1e42313768995
Sahil Ostwal <89473512+Sahilostwal@users.noreply.github.com>
2023-07-13 17:00:19 +0530

    Add files via upload

---
+++ two_sum.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class two_sum{
+    public static void sum(int[] arr,int target){
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]+arr[j]==target){
+                    System.out.print(i + " " + j);
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr={3,3};
+        int target=6;
+        sum(arr, target);
+    }
+}
